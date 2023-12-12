47a10087483cd09680e48bec8e3665885fd21316
radenroro <148857302+radenroro@users.noreply.github.com>
2023-12-12 14:56:58 +0700

    SQL

---
+++ insertskripsi.sql
@@ -0,0 +1,7 @@
+INSERT INTO buku (isbn, judul, subjudul, penulis, penerbit, tahun_terbit, jumlah_halaman)
+VALUES
+('978-623-215-567-1', 'Ada Rasa', '', 'Holifah Nur Azizah', 'Cipta Media Edukasi', 2019, 42),
+('978-623-7555-02-5', 'Ada Sekolah Aneh (?)', '', 'Gustri Yulia...[et al.]', 'Soega Publishing', 2019, 172),
+('978-623-252-264-0', 'Ada Senyum Dibalik Cerita', 'Sriyatun', 'Wahyu Bagus Setiawan', 'Indocamp', 2019, 40),
+('978-623-91973-6-0', 'Ada yang Tak Seberuntung Kita, Tapi Terlihat Lebih Bahagia', 'Mahasiswa rekam jejak IAI UNP', 'Frizka Andani', 'Al-Fannani Publisher', 2019, 190),
+('978-602-5951-70-1', 'Adakah Pancasila di Dirimu?', 'Mahasiswa Pendidikan Teknik Bangunan Unimed Reguler B 2018', 'Pristi Suhendro Lukitoyo', 'Obelia Publisher', 2019, 121);
